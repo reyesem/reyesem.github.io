dabe4555d832fd1aed6d5081cc982ad2fea9629d
Eric Reyes <reyesem@users.noreply.github.com>
2019-03-07 08:54:03 -0500

    Update InstallPkgs.R

--- files/MA482/InstallPkgs.R
+++ files/MA482/InstallPkgs.R
@@ -4,18 +4,20 @@
 # Description: Automate the package installation process.
 #
 # Author: Eric Reyes
-# Date: Spring 2016-2017
+# Date: Spring 2018-2019
 # Modified:
 #
 # Notes:
 
-# Specify Library
-.globallib <- paste(Sys.getenv("R_HOME"), "/library", sep="")
 
 # Load Additional Packages First
-install.packages("tidyverse", lib = .globallib, dependencies = TRUE)
-install.packages("broom", lib = .globallib, dependencies = TRUE)
-install.packages("splines", lib = .globallib, dependencies = TRUE)
-install.packages("geepack", lib = .globallib, dependencies = TRUE)
-install.packages("rms", lib = .globallib, dependencies = TRUE)
-install.packages("lme4", lib = .globallib, dependencies = TRUE)
+install.packages(c("car",
+                   "skimr",
+                   "splines",
+                   "geepack",
+                   "rms",
+                   "lme4",
+                   "tidyverse",
+                   "broom",
+                   "knitr"), dependencies = TRUE)
+
