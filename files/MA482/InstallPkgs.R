066baeabbaf8675408a5364bb86520feb31bc59c
Eric Reyes <reyesem@users.noreply.github.com>
2020-03-06 09:31:32 -0500

    Update InstallPkgs.R

--- files/MA482/InstallPkgs.R
+++ files/MA482/InstallPkgs.R
@@ -19,10 +19,17 @@ install.packages(c("car",
                    "tidyverse",
                    "broom",
                    "broom.mixed",
-                   "knitr"), 
+                   "knitr",
+                   "rstudioapi"), 
                  repos = "https://cloud.r-project.org",
                  dependencies = TRUE,
                  quiet = TRUE)
 
+# ---- Create Directory ----
+ma482dir <- rstudioapi::selectDirectory(
+  caption = "Select directory for course materials.")
+
+download.file("https://github.com/reyesem/reyesem.github.io/raw/master/files/MA482/ReyesStyleDoc.docx",
+              paste0(ma482dir, "/ReyesStyles.docx"))
 
 message("Set-up was successful!")
