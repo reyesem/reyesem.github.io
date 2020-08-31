faf30e3d643f200fa38c2f26507759ec3aaf1ff5
Eric Reyes <reyesem@users.noreply.github.com>
2020-08-31 14:03:09 -0400

    Update 386 Files

---
+++ files/MA386/MA386CourseSetup.R
@@ -0,0 +1,27 @@
+################################################################################
+# File: MA386CourseSetup
+# Course: MA386 Statistical Programming
+# Description: Completes installation of necessary packages.
+#
+# Author: Eric Reyes
+# Date: Fall 2020
+
+# ---- Install CRAN Packages ----
+install.packages(c("tidyverse",
+                   "rvest",
+                   "jsonlite",
+                   "ggmap",
+                   "tidygeocoder",
+                   "plotly",
+                   "learnr",
+                   "knitr"), dependencies = TRUE)
+
+
+
+# ---- Install Guided Notes ----
+install.packages("https://reyesem.github.io/files/MA386/reyes386_0.1.0.tar.gz",
+                 repos = NULL)
+
+
+
+message("\n\nSetup Complete!\n")
