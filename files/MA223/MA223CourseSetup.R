fecd6fbea773cdb3fe6ab2729edf1476559fa7ea
Eric Reyes <reyesem@users.noreply.github.com>
2020-11-24 16:32:08 -0500

    Update MA223 Files

--- files/MA223/MA223CourseSetup.R
+++ files/MA223/MA223CourseSetup.R
@@ -4,15 +4,15 @@
 # Description: Automate the directory set up package installation for the class.
 #
 # Author: Eric Reyes
-# Date: Fall 2021
+# Date: Winter AY2021
 #
 # Notes:
 
 # ---- Package Installation ----
 install.packages("googlesheets", quiet = TRUE)
-
+install.packages("learnr", quiet = TRUE)
 install.packages("https://reyesem.github.io/files/MA223/IntroAnalysis_0.1.0.tar.gz", 
-                 repos = NULL)
+                 repos = NULL, quiet = TRUE)
 
 
 # ---- Create Directory ----
