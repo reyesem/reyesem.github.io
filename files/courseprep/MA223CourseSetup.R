3802b18b6fa8dceb3bab2d3d2c7bae35a223147e
Eric Reyes <reyesem@users.noreply.github.com>
2022-03-17 15:30:19 -0400

    Rose Show
    Include files for 2022 Rose Show

--- files/courseprep/MA223CourseSetup.R
+++ files/courseprep/MA223CourseSetup.R
@@ -14,7 +14,6 @@ install.packages("htmltools", quiet = TRUE)
 install.packages("jquerylib", quiet = TRUE)
 install.packages("xfun", quiet = TRUE)
 install.packages("rmarkdown", quiet = TRUE)
-install.packages("googlesheets", quiet = TRUE)
 install.packages("learnr", quiet = TRUE)
 install.packages(
   "https://github.com/reyesem/reyesem.github.io/raw/master/files/courseprep/IntroAnalysis_0.1.0.tar.gz",
