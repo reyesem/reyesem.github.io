3c7c9f7962384861aef941c63e6144a098472f7e
Eric Reyes <reyesem@users.noreply.github.com>
2024-09-04 00:19:50 -0400

    Update MA382CourseSetup.R

--- files/courseprep/MA382CourseSetup.R
+++ files/courseprep/MA382CourseSetup.R
@@ -1,23 +1,23 @@
 ################################################################################
-# Program: MA223CourseSetup
-# Class: MA223 Engineering Statistics I
+# Program: MA382CourseSetup
+# Class: MA382 Introductory Statistics
 # Description: Automate the directory set up package installation for the class.
 #
 # Author: Eric Reyes
+# Date: Fall 2425
 #
 # Notes:
 
 # ---- Package Installation ----
 install.packages(
-  'png',
-  quiet = TRUE
-)
-
-install.packages(
   "https://github.com/reyesem/reyesem.github.io/raw/master/files/courseprep/IntroAnalysis_0.1.0.tar.gz",
   repos = NULL,
   quiet = TRUE)
 
+install.packages(
+  'quantreg',
+  quiet = TRUE
+)
 
 # ---- Create Directory ----
 dir.create("Individual-Assignments")
